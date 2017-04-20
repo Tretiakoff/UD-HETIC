b39b787f0f1106ea28f5435467ce5dcbdcac0990
Tretiakoff <c.tretiakoff@gmail.com>
2017-04-21 01:00:17 +0200

    wip

--- public/assets/script/sticky-contact.js
+++ public/assets/script/sticky-contact.js
@@ -7,22 +7,32 @@ $(".information").click(function() {
 
 $(".introTel").click(function() {
     $(".telephoneForm").toggleClass("hide");
+    $(".messageForm").addClass("hide");
+    $(".flecheMessage").removeClass("arrow_carrot-down");
+    $(".flecheMessage").addClass("arrow_carrot-right");
     $(".flecheTel").toggleClass("arrow_carrot-down");
     $(".flecheTel").toggleClass("arrow_carrot-right");
 });
 
 $(".introMessage").click(function() {
+    $(".telephoneForm").addClass("hide");
     $(".messageForm").toggleClass("hide");
     $(".stickyGlobal").toggleClass("bgGrand");
     $(".moyen").toggleClass("grand");
+    $(".flecheTel").removeClass("arrow_carrot-down");
+    $(".flecheTel").addClass("arrow_carrot-right");
     $(".flecheMessage").toggleClass("arrow_carrot-down");
     $(".flecheMessage").toggleClass("arrow_carrot-right");
 });
 
 $(".close").click(function() {
     $(".moyen").addClass("hide");
+    $(".flecheTel").removeClass("arrow_carrot-down");
+    $(".flecheTel").addClass("arrow_carrot-right");
     $(".stickyGlobal").removeClass("bgMoyen");
     $(".stickyGlobal").removeClass("bgGrand");
+    $(".flecheMessage").removeClass("arrow_carrot-down");
+    $(".flecheMessage").addClass("arrow_carrot-right");
     $(".telephoneForm").addClass("hide");
     $(".messageForm").addClass("hide");
     $(".moyen").removeClass("grand");
